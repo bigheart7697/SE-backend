9593922008047af77b8423bbd050b145de563253
bigheart7697 <rezabigheart76@gmail.com>
2020-01-31 18:26:11 +0330

    back finished

--- SE_project/apps/accounts/urls.py
+++ SE_project/apps/accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import include
 from rest_framework_jwt.views import obtain_jwt_token
 
 from .views import ProfileDetailView, AdvisorCreateView, StudentCreateView, StudentUpdateView, AdvisorUpdateView, \
-    AdvisorsListView, AdvisorSearchView
+    AdvisorsListView
 
 urlpatterns = [
     url(r'^login/', obtain_jwt_token, name='token-auth'),
@@ -12,5 +12,5 @@ urlpatterns = [
     url(r'^student-edit-profile/', StudentUpdateView.as_view(), name="student edit profile"),
     url(r'^advisor-edit-profile/$', AdvisorUpdateView.as_view(), name="advisor edit profile"),
     url(r'^user/$', ProfileDetailView.as_view(), name="profile"),
-    url(r'^advisors-list/$', AdvisorSearchView.as_view(), name="advisor list")
+    url(r'^advisors-list/$', AdvisorsListView.as_view(), name="advisor list")
 ]
